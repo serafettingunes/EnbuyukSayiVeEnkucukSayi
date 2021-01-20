be5eef7fb61ffffb12360252dbce4ea6c41c33bb
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 18:04:57 +0300

    Add project files.

---
+++ EnkucukEnbuyukSayi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnkucukEnbuyukSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, enbuyuk = 0, enkucuk = 999999999;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Bir sayı girin: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (enbuyuk < x)
+                {
+                    enbuyuk = x;
+                }
+                if (enkucuk > x)
+                {
+                    enkucuk = x;
+                }
+            }
+            Console.WriteLine("en büyük sayı:{0} en küçük sayı:{1}", enbuyuk, enkucuk);
+        }
+    }
+}
